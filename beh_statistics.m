bb146a7d7e82db976c316e039a5879102f464342
Mpachoud <32837324+Mpachoud@users.noreply.github.com>
2019-11-04 17:49:29 +0100

    Update beh_statistics.m

--- beh_statistics.m
+++ beh_statistics.m
@@ -26,7 +26,7 @@ for sr=1:numel(saccades_reaches)
         comparisson=comparissons{co};
         for pa=1:numel(parameters)
             par=parameters{pa}; %the next if condition defines the parameters to NOT calculate statistics 
-            if strcmp(par,'ini_fix') || strcmp(par,'ini_abort') || strcmp(par,'abort_raw_states') || strcmp(par,'abort_raw_time_axis') || strcmp(par,'abort_raw_x') || strcmp(par,'abort_raw_y') || strcmp(par,'abort_fix_pos') || strcmp(par,'abort_tar_pos') || strcmp(par,'success_raw_states') || strcmp(par,'success_raw_time_axis') || strcmp(par,'success_raw_x') || strcmp(par,'success_raw_y') || strcmp(par,'success_fix_pos') || strcmp(par,'success_tar_pos') || strcmp(par,'abort_code') || strcmp(par,'abort_lat') || strcmp(par,'success_lat') ...
+            if strcmp(par,'ini_fix') || strcmp(par,'ini_dur') || strcmp(par,'ini_abort') || strcmp(par,'abort_raw_states') || strcmp(par,'abort_raw_time_axis') || strcmp(par,'abort_raw_x') || strcmp(par,'abort_raw_y') || strcmp(par,'abort_fix_pos') || strcmp(par,'abort_tar_pos') || strcmp(par,'success_raw_states') || strcmp(par,'success_raw_time_axis') || strcmp(par,'success_raw_x') || strcmp(par,'success_raw_y') || strcmp(par,'success_fix_pos') || strcmp(par,'success_tar_pos') || strcmp(par,'abort_code') || strcmp(par,'abort_lat') || strcmp(par,'success_lat') ...
                     || strcmp(par,'abort_trial') || strcmp(par,'abort_run') || strcmp(par,'abort_session') || strcmp(par,'success_trial') || strcmp(par,'success_run') || strcmp(par,'success_session') || strcmp(par,'trial') || strcmp(par,'run') || strcmp(par,'session'), continue, end
             out_stru_te_sr=vertcat(out_stru_te.(par));
             type_effector_fieldnames=fieldnames(out_stru_te_sr);            
