ba3f9a9b3263ebcdff4e0a501c3565774b3467e8
Mpachoud <32837324+Mpachoud@users.noreply.github.com>
2019-11-15 15:20:22 +0100

    Update Linus_PPC_pul_beh.m

--- Settings/MIP_dPul_inj_choi_w_o_20171012/Linus_PPC_pul_beh.m
+++ Settings/MIP_dPul_inj_choi_w_o_20171012/Linus_PPC_pul_beh.m
@@ -36,7 +36,7 @@ GLO.only_significant                =   1; % for sigstar
 GLO.only_success_for_accuracy       =   0;
 GLO.only_between_group_comparissons =   0;
 GLO.point_per_batch                 =   0; %0 average across session , 1 % 1 point per run
-GLO.summary                         =   [1 2 3 5 10]; %which plots (vector of number of -1 for all plots)
+GLO.summary                         =   [1 2 3 5 10 12]; %which plots (vector of number of -1 for all plots) [1 2 3 5 10]
 GLO.target_locations_in_raw         =   0; %in raw plots, also plot target location
 GLO.saccade_in_raw                  =   0; %only for one plot, see beh_compare_groups
 GLO.modify_positions                =   0; % used in reallocate_positions_from_mpa ?
@@ -46,9 +46,7 @@ GLO.keep_raw_output                 =   1; % 1 = save raw data in the output str
 GLO.hits_in_plot                    =   1; % plot the number of hits per condition on plots
 GLO.min_hits                        =   0; %or 1 for 50 hits min
 GLO.only_successful_side_selection  =   0; %0 takes in account aborted trial from CUE_ON, 1 only successful trials
-GLO.compute_reach_1st_pos           =   0;
-GLO.compute_reach_1st_pos_in        =   1;
-GLO.compute_reach_pos_at_state_change = 0;  
+
 
 % steady.passfilter                   =   {'saccades','lat',0.08, 0.5;'reaches','lat',0.1, 10};
 
@@ -76,8 +74,8 @@ GLO.remove_outliers                 =   0;
 % % 3: last saccade in the state
 % % 4: first saccade in the state
 % % 5: the first that is bigger than 'sac_min_amp'
-steady.reach_1st_pos                =   1; %%new used for all but
-steady.reach_1st_pos_in             =   0; %%new
+steady.reach_1st_pos                =   0; %%new used for all but
+steady.reach_1st_pos_in             =   1; %%new
 steady.reach_pos_at_state_change    =   0; %%new
 
 steady.remove_outliers              =   GLO.remove_outliers;
